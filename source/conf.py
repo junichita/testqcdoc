82400c0f5071c2bc735235bb75d69ad7eeb8651c
Junichi TANAKA <Junichi.Tanaka@cern.ch>
2020-05-09 01:50:19 +0900

    fix a theme

--- source/conf.py
+++ source/conf.py
@@ -43,10 +43,17 @@ language = 'ja'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
 
 # -- Options for HTML output -------------------------------------------------
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
@@ -58,8 +65,3 @@ from PSphinxTheme import utils
 
 p, html_theme, needs_sphinx = utils.set_psphinxtheme(html_theme)
 html_theme_path = p
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
